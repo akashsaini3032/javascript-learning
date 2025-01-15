73ea32f5e5fa5c6a29784bb16b9408d5fec0a3b8
akashsaini3032 <akashsainira@gmail.com>
2025-01-15 23:24:59 +0530

    update content

---
+++ day2homework.js
@@ -0,0 +1,40 @@
+function okk(){
+    console.log("okk");
+}
+
+
+
+function ak(){
+
+    console.log("ak");
+}
+
+
+
+function jk(){
+
+    console.log("jk");
+}
+
+
+
+
+function mj(){
+
+    console.log("mj");
+}
+
+
+
+function an(){
+
+    console.log("an");
+}
+
+
+
+okk();
+ak();
+jk();
+mj();
+an();
---
+++ day3.js
@@ -0,0 +1,65 @@
+//   int main(){
+
+
+//     return 0;
+//   }
+
+
+function okk(){
+
+    console.log("First");
+    
+}
+
+function go(){
+    // let a
+// let  const
+
+// Declare
+    // var a=5;
+    // var a=7;
+    // console.log(a);
+
+    // let a=5
+    // let a=6
+
+
+    // reassign
+
+    // var a=15;
+    //   a=20; // reassign  
+    // console.log(a);
+    
+    // let a=18;
+    // a=20
+    // console.log(a);
+
+
+    // const a=13;
+    // a=15;
+    // console.log(a);
+        
+
+//    let a=20;
+//    {
+//      let a=25
+//        console.log("Inner"+a);  
+//    }    
+//    console.log("Outer"+a);
+
+
+
+   // Hoisting
+   console.log(a);
+
+   var a=56;
+
+   
+   
+
+
+    
+}
+
+go()
+// okk();
