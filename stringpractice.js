23b9c1fee13f5135ec55c02d9817149e8b957361
akashsaini3032 <akashsainira@gmail.com>
2025-01-21 09:29:13 +0530

    update content

---
+++ stringpractice.js
@@ -0,0 +1,25 @@
+function string(){
+    //replaceAll()
+    //let text="akash ankit ganesh amaan";
+    //let newtext=text.replaceAll("akash","saini");
+    //document.write(newtext) //output akash change with saini
+
+    //charAT()
+    //let text="hello, World!";
+    //let char=text.charAt(7);
+    //document.write(char); //output "W"
+
+    // split()
+    //let text= "red,green,blue";
+    //let colors=text.split(",");
+    //document.write(colors); //output "red," "green","blue"
+
+    //indexOf()
+    let text="javascript is fun";
+    let index =text.indexOf("fun");
+    document.write(index); //output 15
+
+
+
+
+}
