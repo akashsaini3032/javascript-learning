cac871d75936359d4bd03667245105eb6ffa14cc
akashsaini3032 <akashsainira@gmail.com>
2025-01-21 22:45:06 +0530

    update content

--- stringpractice.js
+++ stringpractice.js
@@ -15,9 +15,26 @@ function string(){
     //document.write(colors); //output "red," "green","blue"
 
     //indexOf()
-    let text="javascript is fun";
-    let index =text.indexOf("fun");
-    document.write(index); //output 15
+    //let text="javascript is fun";
+    //let index =text.indexOf("fun");
+    //document.write(index); //output 15
+
+    //day2
+    // array is the combination of same type of data
+    //split is used to convert string into array
+    //let abc="this is string"
+    //
+    //console.log(abc.split(" ")); //her " space " is separater
+
+
+    //replace
+    let abc="Cybrom is located at cybrom and Cybrom is good"
+    document.write(abc.replace(/Cybrom/gi,"Mp-Nagar")) //g for global i for case incentitive
+
+    // trim aage and piche ka extra part remove kar deta hai
+
+
+
 
 
 
