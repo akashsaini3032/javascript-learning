19486040ca790b286d084ec60a2f5e3c575cca2e
akashsaini3032 <akashsainira@gmail.com>
2025-01-23 14:24:00 +0530

    update content

---
+++ array.js
@@ -0,0 +1,39 @@
+function array(){
+    let fruits=["apple","mango","banana","papaya"] //array
+   // document.write(fruits.length) //length of fruit
+    //document.write(fruits[2]) //access
+    //fruits.pop() // remove last element papaya remove
+    //document.write(fruits) papaya remove hokar print ho jayega
+
+    //fruits.shift() //remove first element apple
+
+    //fruits.push("grapes","orange") //addd element to end
+    //document.write(fruits)
+
+    //fruits.unshift("peach") //add new element to start
+    //document.write(fruits)
+
+
+
+    //day2
+    //document.write(fruits.slice(2,4)) // output banana,4-1=3papaya 
+
+    //splice
+    //fruits.splice(1,0,"grapes") // 1index number where add ,0 number how much delete, grapes what add
+    //document.write(fruits)
+
+    //slice me original array me koi changes nahi hota hai print karke dekhte hai
+    //baki jo array me padha hai usme original array me changes hota hai
+
+    //reverse
+    //fruits.reverse() reverse array
+    //document.write(fruits)
+
+    let abc=[1,5,7,3,8]
+    //document.write(abc.sort()) // arrange in ascending
+    //document.write(abc.sort().reverse()) // arrange in ascending .reverse() lagane se sort ke baad reverse ho jayega
+
+    
+
+
+}
