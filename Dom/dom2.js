ef2dbc524c034d3504d95380b7d60d5182ec43bd
akashsaini3032 <akashsainira@gmail.com>
2025-01-24 23:14:53 +0530

    update content

---
+++ Dom/dom2.js
@@ -0,0 +1,22 @@
+function saini(){
+    let show=document.querySelector(".akash")
+
+    
+    show.style.backgroundColor="red"
+}
+
+function ak(){
+    let show=document.querySelector(".akash")
+
+    
+    show.style.backgroundColor="green"
+}
+
+function jk(){
+    let show=document.querySelector(".akash")
+
+    
+    
+    show.style.backgroundImage="url(blog1.jpg)"
+}
+
