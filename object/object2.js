51d2e99bafc629c4d5612ae72da421062fdb7801
akashsaini3032 <akashsainira@gmail.com>
2025-01-31 23:54:13 +0530

    update content

---
+++ object/object2.js
@@ -0,0 +1,28 @@
+function ak(){
+   // const student={name:"john",age:25,city:"new york"};
+//const{name,age}=student;
+
+//document.write(name);
+
+
+
+
+}
+
+//yah console pe show hoga object screen pe show nahi hota document.write kam nahi karega isme
+
+const student1={name:"john",age:25};
+const student2={city:"bhopal",grade:"A"};
+const combinedstudent={...student1,...student2};
+
+console.log(combinedstudent);
+
+
+
+//function
+//normal
+
+//arrow is special type of aanomonious function
+
+
+
