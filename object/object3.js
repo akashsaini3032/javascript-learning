067269ea16d7cd3d0fdda5b382ee213dbcb0c021
akashsaini3032 <akashsainira@gmail.com>
2025-02-01 22:49:30 +0530

    update content

---
+++ object/object3.js
@@ -0,0 +1,22 @@
+//normal function
+//function okk(){
+//console.log("normal function");
+
+//}
+//okk()
+
+
+//anonymus Function
+//let go=function(){
+//console.log("Anonymus function");
+//}
+
+//go()
+
+
+//IIFE
+(
+    function(){
+        console.log("IIFE");
+    }
+)()
