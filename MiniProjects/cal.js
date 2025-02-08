c27dfcccf61968af3e976bb8d03c89531bfac8fc
akashsaini3032 <akashsainira@gmail.com>
2025-02-08 19:53:02 +0530

    update content

---
+++ MiniProjects/cal.js
@@ -0,0 +1,31 @@
+let store='';
+let dis=(value)=>{
+    let show=document.querySelector("#display")
+    store=store+value
+    show.innerHTML=store
+}
+
+let res=()=>{
+
+    let show=document.querySelector("#display")
+
+    //eval  predefined function hai jo calculation karta hai
+    show.innerHTML=eval(store) //string => number
+    store=eval(store) //number 
+    store.toString() //string me store karana
+
+}
+
+let del=()=>{
+    store=store.slice(0,-1)
+    let show=document.querySelector("#display")
+    show.innerHTML=store
+}
+
+let ac=()=>{
+    store=""
+    let show=document.querySelector("#display")
+    show.innerHTML=store
+}
+
+//event delegatiom event bubling 
