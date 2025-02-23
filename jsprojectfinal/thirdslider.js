92eaf6a0e36fc973c212ab3b9cf7d802aa694c9c
akashsaini3032 <akashsainira@gmail.com>
2025-02-24 00:13:48 +0530

    update content

---
+++ jsprojectfinal/thirdslider.js
@@ -0,0 +1,13 @@
+var uniqueSwiper = new Swiper(".unique-swiper-container", {
+    speed: 600,
+    parallax: true,
+    pagination: {
+      el: ".unique-swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".unique-swiper-button-next",
+      prevEl: ".unique-swiper-button-prev",
+    },
+  });
+  
