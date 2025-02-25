430f48cacd27503d769c087fd9bfed443004e01b
akashsaini3032 <akashsainira@gmail.com>
2025-02-25 22:51:23 +0530

    update content

---
+++ projectjavascript/firstslider.js
@@ -0,0 +1,9 @@
+//first slider javascript code
+  
+    var swiper = new Swiper(".mySwiper", {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  
