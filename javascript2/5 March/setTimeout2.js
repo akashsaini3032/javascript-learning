6f7db9bfcd2f03ff2911523999c183913c045b9d
akashsaini3032 <akashsainira@gmail.com>
2025-03-05 23:30:40 +0530

    update content

---
+++ javascript2/5 March/setTimeout2.js
@@ -0,0 +1,14 @@
+// setTimeout(()=>{
+//     let head=document.createElement('h1')
+//     head.textContent="Welcome to World"
+//     let bd=document.body
+//     bd.append(head)
+    
+
+// },3000)
+setTimeout(()=>{
+   let h1=document.querySelector('#head')
+   h1.style.display = "block"
+    
+
+},3000)
