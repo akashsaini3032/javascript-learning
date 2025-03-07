89797ac073bed6fdf05a8e5fd8de277f05018f5b
akashsaini3032 <akashsainira@gmail.com>
2025-03-07 20:58:11 +0530

    update content

--- JAVASCRIPT2.0/6 March/form2.js
+++ JAVASCRIPT2.0/6 March/form2.js
@@ -2,6 +2,8 @@ function validate(){
     let name=document.querySelector('#name').value
     let email=document.querySelector('#email').value
     let number=document.querySelector('#number').value
+    let pass=document.querySelector('#pass').value
+    let contact=document.querySelector('#contact').value
 
     if(name==""){
         let errorname=document.querySelector('#errname')
@@ -17,11 +19,13 @@ function validate(){
         return false
     }
 
-    else if (!(email.includes('@'))){
-        alert("please enter valid email must contain '@")
+    else if (!(email.includes('@') && email.includes('.com'))){
+        alert("please enter valid email must contain '@' and '.com'")
         document.querySelector('#email').focus()
         return false
     }
+
+    
 
     else if(number==""){
         let errorage=document.querySelector('#errage')
@@ -30,9 +34,50 @@ function validate(){
         return false
     }
 
-    else if( isNaN(age)){
-        alert("please your age innumber")
-        document.querySelector('#age').focus()
+    else if(number>=100 && number<1){
+        let errorage=document.querySelector('#errage')
+        errorage.innerHTML="Please Enter Your age between 1to 1oo"
+        document.querySelector('#number').focus()
         return false
     }
+
+    else if( isNaN(age)){
+        alert("please your age innumber")
+        document.querySelector('#number').focus()
+        return false
+    }
+
+
+    else if(pass==""){
+        let errorpass=document.querySelector('#errpass')
+        errorpass.innerHTML="Please Enter Your Password"
+        document.querySelector('#pass').focus()
+        return false
+    }
+
+    else if(!(pass.match(/[!@#$%^&*()_+]/))){
+        alert("please enter your password with special character")
+        document.querySelector('#pass').focus()
+        return false
+    }
+
+    else if(contact==""){
+        let errorcontact=document.querySelector('#errcontact')
+        errorcontact.innerHTML="Please Enter Your contact"
+        document.querySelector('#contact').focus()
+        return false
+    }
+
+    else if(contact.length>10 || contact.length<10){
+
+        alert("Please Enter Your contact in 10 digit")
+        document.querySelector('#contact').focus()
+        return false
+
+    }
+
+    
+
+
+
 }
