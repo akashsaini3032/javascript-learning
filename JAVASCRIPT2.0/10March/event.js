736b121314fc1d84738fba7d0ace3549a1c417f1
akashsaini3032 <akashsainira@gmail.com>
2025-03-10 20:01:35 +0530

    update content

---
+++ JAVASCRIPT2.0/10March/event.js
@@ -0,0 +1,25 @@
+let btn=document.querySelector('#btn')
+
+btn.addEventListener('click',()=>{alert("Hello")})
+
+
+let btn2=document.querySelector('#btn2')
+
+btn2.addEventListener('dblclick',()=>{alert("Hyy")})
+
+
+let btn3=document.querySelector('#btn2')
+
+btn3.addEventListener('mouseenter',()=>{alert("Hyy")})
+
+
+let btn4=document.querySelector('#btn2')
+
+btn4.addEventListener('mouseleave',()=>{alert("Hyy")})
+
+
+
+
+
+
+
---
+++ JAVASCRIPT2.0/Array/array1.js
@@ -0,0 +1,18 @@
+// //for values
+// let ar=[12,34.5,'cybrom',true]
+// for(let vl of ar){
+//     console.log(vl);
+// }
+
+// //for index value
+// for(let k in ar){
+//     console.log(k);
+// }
+
+
+let num=prompt("enter number ")
+
+let ar=[num]
+
+console.log(ar);
+
