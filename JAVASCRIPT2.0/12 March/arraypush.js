1c1d9cb6941759ed6d6f42b5b37d0429b47905ff
akashsaini3032 <akashsainira@gmail.com>
2025-03-12 22:46:10 +0530

    update content

---
+++ JAVASCRIPT2.0/12 March/arraypush.js
@@ -0,0 +1,12 @@
+let aar=[2,3,4,5,6]
+aar.push("last element")
+console.log(aar)
+
+aar.unshift("first element","second element")
+console.log(aar)
+
+aar.pop()
+console.log(aar)
+
+aar.shift()
+console.log(aar)
---
+++ JAVASCRIPT2.0/12 March/arrayslice.js
@@ -0,0 +1,3 @@
+let aar=["Raghav",24,12313,"Bhopal","FSD"]
+let part=aar.slice(1,4)
+console.log(part)
