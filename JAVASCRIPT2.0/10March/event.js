bd2e2d02ea3a0f1c9b4ed5bdcd282b808b0f7ddf
akashsaini3032 <akashsainira@gmail.com>
2025-03-16 22:43:35 +0530

    update content

--- JAVASCRIPT2.0/10March/event.js
+++ JAVASCRIPT2.0/10March/event.js
@@ -1,21 +1,22 @@
-let btn=document.querySelector('#btn')
+let btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    alert("Hello");
+});
 
-btn.addEventListener('click',()=>{alert("Hello")})
+let btn2 = document.querySelector('#btn2');
+btn2.addEventListener('dblclick', () => {
+    alert("Hyy");
+});
 
+let btn3 = document.querySelector('#btn3'); // Fixed to target #btn3
+btn3.addEventListener('mouseenter', () => {
+    alert("Hyy");
+});
 
-let btn2=document.querySelector('#btn2')
-
-btn2.addEventListener('dblclick',()=>{alert("Hyy")})
-
-
-let btn3=document.querySelector('#btn2')
-
-btn3.addEventListener('mouseenter',()=>{alert("Hyy")})
-
-
-let btn4=document.querySelector('#btn2')
-
-btn4.addEventListener('mouseleave',()=>{alert("Hyy")})
+let btn4 = document.querySelector('#btn4'); // Fixed to target #btn4
+btn4.addEventListener('mouseleave', () => {
+    alert("Hyy");
+});
 
 
 
--- JAVASCRIPT2.0/Array/array1.js
+++ JAVASCRIPT2.0/Array/array1.js
@@ -1,23 +1,13 @@
 // //for values
-// let ar=[12,34.5,'cybrom',true]
-// for(let vl of ar){
-//     console.log(vl);
-// }
+let ar=[12,34.5,'cybrom',true]
+for(let vl of ar){
+    console.log(vl);
+}
 
 // //for index value
-// for(let k in ar){
-//     console.log(k);
-// }
+for(let k in ar){
+    console.log(k);
+}
 
 
-let input=prompt("Enter array elements with space")
-input.split(" ");
-console.log(input);
-
-
-let num=prompt("enter number ")
-
-let ar=[num]
-
-console.log(ar);
 
