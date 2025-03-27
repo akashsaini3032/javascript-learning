0c2a0991cf9abb7b082da9772ae4fc625cbce289
akashsaini3032 <akashsainira@gmail.com>
2025-03-27 22:28:29 +0530

    update content

---
+++ JAVASCRIPT2.0/API/api.js
@@ -0,0 +1,18 @@
+//API ka kaam service ko webpage pe fetch karna 
+
+//api ko intergrated karane ke liye fetch function karatenhai iske andar twp argument aata hai
+//1 url 2 object
+
+//fetch function ka kaam
+
+// get,post,delete,put(update) // yah sab request hai https request ya method
+
+
+//sync  line by line chalta hai
+//      //async // jo pahle complete ho jaye wah work karne lagega jo sabse pahle complete hoga wah sabse pahle execute hoga
+
+//javascript lightweight hai sync hai but api use karte hai to async ho jati hai to wait nahi karti 
+// isiliye 
+// async //function me 
+//await 
+//use karte hai
---
+++ JAVASCRIPT2.0/String/string1.js
@@ -0,0 +1,37 @@
+// string=> collection of characters
+
+let st="Welcome"
+
+console.log(st[4]);
+
+//template literals ``
+// template letarls use karte hai javascript me html code likhne ke liye
+// template letarls me javascript ke sath html code likh sakte hai
+// template letarls me jo code likh denge wahi same to same print ho jayega
+
+let name ="akash"
+
+
+
+let name1=`my name is ${name}
+saini`
+console.log(name1);
+
+// to uppercase
+
+
+let ak="akka"
+
+console.log(ak.toUpperCase());
+
+
+//to lower case
+
+let bk="ASDHJJ"
+
+console.log(bk.toLowerCase());
+
+//repeat
+
+let r=bk.repeat(3) // yah use karte hai repeat karne ke liye
+console.log(r);
