a5c00ac40eec0535786a9c2ff266bb7126442fdb
akashsaini3032 <akashsainira@gmail.com>
2025-04-07 22:36:08 +0530

    json practice

--- Crud/script.js
+++ Crud/script.js
@@ -1,36 +1,36 @@
-let fetchData= async()=>{
-    let url="http://localhost:3000/carrental"
-    let res= await fetch(url, {method:"GET"}) 
+// let fetchData= async()=>{
+//     let url="http://localhost:3000/carrental"
+//     let res= await fetch(url, {method:"GET"}) 
 
-    let data= await res.json()
+//     let data= await res.json()
 
-    console.log(data);
+//     console.log(data);
 
-    let Show=document.querySelector("#datashow")
+//     let Show=document.querySelector("#datashow")
 
-    data.map(
-        (e)=>{
+//     data.map(
+//         (e)=>{
 
-            Show.innerHTML+=`
+//             Show.innerHTML+=`
     
-            <tr>
-                <td> ${e.name}</td>
-                <td> ${e.age}</td>
-                <td> ${e.mobile}</td>
-                <td> ${e.person}</td>
-                <td> ${e.price}</td>
-                <td onclick="del('${e.id}')"> delete </td>
-            </tr>
+//             <tr>
+//                 <td> ${e.name}</td>
+//                 <td> ${e.age}</td>
+//                 <td> ${e.mobile}</td>
+//                 <td> ${e.person}</td>
+//                 <td> ${e.price}</td>
+//                 <td onclick="del('${e.id}')"> delete </td>
+//             </tr>
             
-            `
-        }
-    )
-}
+//             `
+//         }
+//     )
+// }
 
-let del=(id)=>{
-    let url=`http://localhost:3000/carrental/${id}`
-    fetch(url, {method:"DELETE"}) 
+// let del=(id)=>{
+//     let url=`http://localhost:3000/carrental/${id}`
+//     fetch(url, {method:"DELETE"}) 
 
-}
+// }
 
-fetchData()
+// fetchData()
