c2aa77be0f68f94746e2b56dda8d9c6037aadd90
akashsaini3032 <akashsainira@gmail.com>
2025-04-12 15:38:57 +0530

    update content

--- game2.js
+++ game2.js
@@ -1,24 +1,24 @@
-// function game() {
-//     let chances = 3; 
+function game() {
+    let chances = 3; 
 
-//     while (chances > 0) {
-//         let value = prompt("Guess your number (You have " + chances + " chances)");
+    while (chances > 0) {
+        let value = prompt("Guess your number (You have " + chances + " chances)");
 
     
-//         value = Number(value);
+        value = Number(value);
 
-//         if (value === 10) {
-//             alert("You won!");
-//             return; 
-//         } else if (value > 10) {
-//             alert("Too high!");
-//         } else if (value < 10) {
-//             alert("Too low!");
-//         }
+        if (value === 10) {
+            alert("You won!");
+            return; 
+        } else if (value > 10) {
+            alert("Too high!");
+        } else if (value < 10) {
+            alert("Too low!");
+        }
 
-//         chances--; 
-//     }
+        chances--; 
+    }
 
 
-//     alert("You lost!");
-// }
+    alert("You lost!");
+}
