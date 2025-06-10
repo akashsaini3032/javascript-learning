4776c744d7908eb1454554f36ae22fc761e1409b
akashsaini3032 <akashsainira@gmail.com>
2025-06-10 23:50:49 +0530

    practice test question

--- test2/1f.js
+++ test2/1f.js
@@ -1,14 +1,14 @@
-// function ak(){
-//     let num=Number(prompt("enter the number"));
-//     if(num<1){
-//         alert("this number is negative")
-//     }
+function ak(){
+    let num=Number(prompt("enter the number"));
+    if(num<1){
+        alert("this number is negative")
+    }
 
-//     else if(num=0){
-//         alert("this number is Zero")
-//     }
+    else if(num=0){
+        alert("this number is Zero")
+    }
 
-//     else if(num>1){
-//         alert("this number is Positive")
-//     }
-// }
+    else if(num>1){
+        alert("this number is Positive")
+    }
+}
