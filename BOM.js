dec64acc5b09c3da528e0d8a9dde8b759e2a8f89
akashsaini3032 <akashsainira@gmail.com>
2025-06-15 23:33:17 +0530

    cal practice

--- BOM.js
+++ BOM.js
@@ -1,17 +1,17 @@
-// function akash(){
-//     window.alert("iamakash")
-// }
+function akash(){
+    window.alert("iamakash")
+}
 
 
-// function saini(){
-//     let name= window.prompt("did you like my website")
-//     window.alert(name)
-// }
+function saini(){
+    let name= window.prompt("did you like my website")
+    window.alert(name)
+}
 
-// function ra(){
-//     let ans= window.confirm("are you 18+")
-//     window.alert(ans)
-// }
+function ra(){
+    let ans= window.confirm("are you 18+")
+    window.alert(ans)
+}
 
 function mj(){
     window.alert("This page is made for practice BOM in javascript")
