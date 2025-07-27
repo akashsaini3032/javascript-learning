d8d068086c6742cfb701df829661229facc56208
akashsaini3032 <akashsainira@gmail.com>
2025-07-27 23:13:38 +0530

    set intervl

--- JAVASCRIPT2.0/setinterval.js
+++ JAVASCRIPT2.0/setinterval.js
@@ -4,16 +4,16 @@
 //let count=0;
 //setInterval(()=>{document.write("count++")},2000)
 
-let st
+// let st
 
-let ct =0
-function fun (){
-    let h=document.querySelector("#count")
-    st=setInterval(()=>{
-        h.innerHTML=ct++
-    },500)
-}
+// let ct =0
+// function fun (){
+//     let h=document.querySelector("#count")
+//     st=setInterval(()=>{
+//         h.innerHTML=ct++
+//     },500)
+// }
 
-function stop(){
-    clearInterval(st)
-}
+// function stop(){
+//     clearInterval(st)
+// }
