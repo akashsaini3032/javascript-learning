d4e1bc154e3c1fe55b18707676ea8158d81d525b
akashsaini3032 <akashsainira@gmail.com>
2025-07-27 23:12:13 +0530

    calculator js

--- MiniProjects/cal.js
+++ MiniProjects/cal.js
@@ -1,31 +1,31 @@
-// let store='';
-// let dis=(value)=>{
-//     let show=document.querySelector("#display")
-//     store=store+value
-//     show.innerHTML=store
-// }
+let store='';
+let dis=(value)=>{
+    let show=document.querySelector("#display")
+    store=store+value
+    show.innerHTML=store
+}
 
-// let res=()=>{
+let res=()=>{
 
-//     let show=document.querySelector("#display")
+    let show=document.querySelector("#display")
 
-//     //eval  predefined function hai jo calculation karta hai
-//     show.innerHTML=eval(store) //string => number
-//     store=eval(store) //number 
-//     store.toString() //string me store karana
+    //eval  predefined function hai jo calculation karta hai
+    show.innerHTML=eval(store) //string => number
+    store=eval(store) //number 
+    store.toString() //string me store karana
 
-// }
+}
 
-// let del=()=>{
-//     store=store.slice(0,-1)
-//     let show=document.querySelector("#display")
-//     show.innerHTML=store
-// }
+let del=()=>{
+    store=store.slice(0,-1)
+    let show=document.querySelector("#display")
+    show.innerHTML=store
+}
 
-// let ac=()=>{
-//     store=""
-//     let show=document.querySelector("#display")
-//     show.innerHTML=store
-// }
+let ac=()=>{
+    store=""
+    let show=document.querySelector("#display")
+    show.innerHTML=store
+}
 
 //event delegatiom event bubling 
