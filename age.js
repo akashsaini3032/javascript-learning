4342b70f09c3f013d7fff38c1461a8684394d7f3
akashsaini3032 <akashsainira@gmail.com>
2025-09-30 23:55:32 +0530

    practice

--- age.js
+++ age.js
@@ -1,8 +1,8 @@
-// function age(){
-//     let age = prompt("Please enter your age:");
-// age = parseInt(age);  
+function age(){
+    let age = prompt("Please enter your age:");
+age = parseInt(age);  
 
 
-// alert(age >= 18 ? "You are an adult." : "You are not an adult.");
+alert(age >= 18 ? "You are an adult." : "You are not an adult.");
 
-// }
+}
