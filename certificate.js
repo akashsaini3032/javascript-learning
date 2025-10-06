af1178fff3772135637dd1a176d51d3d58cfcb50
akashsaini3032 <akashsainira@gmail.com>
2025-10-06 23:12:47 +0530

    certificate

--- certificate.js
+++ certificate.js
@@ -1,9 +1,9 @@
-function certificate(){
-    let hasCertificate = confirm("Have you completed the course and received a certificate");
+// function certificate(){
+//     let hasCertificate = confirm("Have you completed the course and received a certificate");
 
-if (hasCertificate) {
-    alert("Great You have completed the course and received your certificate.");
-} else {
-    alert("It looks like you haven't completed the course or received a certificate yet.");
-}
-}
+// if (hasCertificate) {
+//     alert("Great You have completed the course and received your certificate.");
+// } else {
+//     alert("It looks like you haven't completed the course or received a certificate yet.");
+// }
+// }
