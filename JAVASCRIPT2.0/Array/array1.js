d84a8738de0be476e71791a7fa161b2045c5e6e7
akashsaini3032 <akashsainira@gmail.com>
2025-10-13 23:22:32 +0530

    practice

--- JAVASCRIPT2.0/Array/array1.js
+++ JAVASCRIPT2.0/Array/array1.js
@@ -1,13 +1,13 @@
 // //for values
-let ar=[12,34.5,'cybrom',true]
-for(let vl of ar){
-    console.log(vl);
-}
+// let ar=[12,34.5,'cybrom',true]
+// for(let vl of ar){
+//     console.log(vl);
+// }
 
-// //for index value
-for(let k in ar){
-    console.log(k);
-}
+// // //for index value
+// for(let k in ar){
+//     console.log(k);
+// }
 
 
 
