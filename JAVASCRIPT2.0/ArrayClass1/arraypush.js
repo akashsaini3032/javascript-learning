1159a3bb666d5ae1c49a82d98a9a97417f7b007b
akashsaini3032 <akashsainira@gmail.com>
2025-10-14 22:39:03 +0530

    javascript practice

--- JAVASCRIPT2.0/ArrayClass1/arraypush.js
+++ JAVASCRIPT2.0/ArrayClass1/arraypush.js
@@ -1,12 +1,12 @@
-// let aar=[2,3,4,5,6]
-// aar.push("last element")
-// console.log(aar)
+let aar=[2,3,4,5,6]
+aar.push("last element")
+console.log(aar)
 
-// aar.unshift("first element","second element")
-// console.log(aar)
+aar.unshift("first element","second element")
+console.log(aar)
 
-// aar.pop()
-// console.log(aar)
+aar.pop()
+console.log(aar)
 
-// aar.shift()
-// console.log(aar)
+aar.shift()
+console.log(aar)
