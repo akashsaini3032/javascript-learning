48fdd5e06e460cbb337e900730f12025ead22c55
akashsaini3032 <akashsainira@gmail.com>
2025-10-19 23:02:48 +0530

    practice

--- JAVASCRIPT2.0/ArrayClass1/arrayslice2.js
+++ JAVASCRIPT2.0/ArrayClass1/arrayslice2.js
@@ -1,9 +1,9 @@
-let ar1=[1,2,3,45,67,5,89,10,34,76,87]
-let part=ar1.slice(1,10)
-for(let i=0;i<11;i++)
-{
-    if(part[i]%2==0)
-    {
-        console.log(part[i])
-    }
-}
+// let ar1=[1,2,3,45,67,5,89,10,34,76,87]
+// let part=ar1.slice(1,10)
+// for(let i=0;i<11;i++)
+// {
+//     if(part[i]%2==0)
+//     {
+//         console.log(part[i])
+//     }
+// }
