330e2cdd06288db7572c8a13f39fe7558e5637d2
akashsaini3032 <akashsainira@gmail.com>
2025-10-21 22:38:22 +0530

    age calculator

--- ImportantTopics/agecalculator.js
+++ ImportantTopics/agecalculator.js
@@ -12,14 +12,14 @@
     //}, 1000);
 //}
 
-let age = () => {
-    let inpnumber = document.querySelector("#number").value;
-    let time = new Date();
-    let count = (time.getFullYear()) - inpnumber;
+// let age = () => {
+//     let inpnumber = document.querySelector("#number").value;
+//     let time = new Date();
+//     let count = (time.getFullYear()) - inpnumber;
 
-    let saw = document.querySelector("#showw");
-    saw.innerHTML = count;
-}
+//     let saw = document.querySelector("#showw");
+//     saw.innerHTML = count;
+// }
 
 
 
