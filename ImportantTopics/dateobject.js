2404bc255f5847df32c1fb3063a2eecf349ca36b
akashsaini3032 <akashsainira@gmail.com>
2025-10-23 22:48:19 +0530

    practice

--- ImportantTopics/dateobject.js
+++ ImportantTopics/dateobject.js
@@ -21,9 +21,9 @@ console.log(arr[time.getDay()]);
 
 let showtime=document.querySelector("#show")
 
-setInterval(() => {
-  let time=new Date()
+// setInterval(() => {
+//   let time=new Date()
         
-showtime.innerHTML=` ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+// showtime.innerHTML=` ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
         
-    }, 1000);
+//     }, 1000);
