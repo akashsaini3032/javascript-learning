4779a095ed7d0bc043e9c58eb982ef5d074a36a7
akashsaini3032 <akashsainira@gmail.com>
2025-12-02 22:16:33 +0530

    practice

--- ImportantTopics/settimeout.js
+++ ImportantTopics/settimeout.js
@@ -27,18 +27,18 @@ let st=()=>{
 }
 
 
-let stop = () => {
-    let inpnumber = document.querySelector("#number").value;
-    let count = inpnumber;
+// let stop = () => {
+//     let inpnumber = document.querySelector("#number").value;
+//     let count = inpnumber;
 
-    let inter = setInterval(() => {
-        let saw = document.querySelector("#showw");
-        saw.innerHTML = count;
+//     let inter = setInterval(() => {
+//         let saw = document.querySelector("#showw");
+//         saw.innerHTML = count;
 
-        if (count <= 0) {
-            clearInterval(inter); // Stop the interval when count reaches zero
-        } else {
-            count--;
-        }
-    }, 1000);
-}
+//         if (count <= 0) {
+//             clearInterval(inter); // Stop the interval when count reaches zero
+//         } else {
+//             count--;
+//         }
+//     }, 1000);
+// }
