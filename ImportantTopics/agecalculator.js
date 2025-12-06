9187319e2bfaa3702ac9ae9c1c1456666a464f85
akashsaini3032 <akashsainira@gmail.com>
2025-12-06 23:15:06 +0530

    practice

--- ImportantTopics/agecalculator.js
+++ ImportantTopics/agecalculator.js
@@ -1,25 +1,27 @@
-//let age = () => {
-    //let inpnumber = document.querySelector("#number").value;
-    //let time= new Date()
-    //let count=(time.getFullYear())-inpnumber
+
+
+let age = () => {
+    let inpnumber = document.querySelector("#number").value;
+    let time= new Date()
+    let count=(time.getFullYear())-inpnumber
     
 
-    //let inter =setInterval(() => {
-        ////let saw = document.querySelector("#showw");
-        //saw.innerHTML = count;
+    let inter =setInterval(() => {
+        //let saw = document.querySelector("#showw");
+        saw.innerHTML = count;
 
     
-    //}, 1000);
-//}
+    }, 1000);
+}
 
-// let age = () => {
-//     let inpnumber = document.querySelector("#number").value;
-//     let time = new Date();
-//     let count = (time.getFullYear()) - inpnumber;
+let age = () => {
+    let inpnumber = document.querySelector("#number").value;
+    let time = new Date();
+    let count = (time.getFullYear()) - inpnumber;
 
-//     let saw = document.querySelector("#showw");
-//     saw.innerHTML = count;
-// }
+    let saw = document.querySelector("#showw");
+    saw.innerHTML = count;
+}
 
 
 
