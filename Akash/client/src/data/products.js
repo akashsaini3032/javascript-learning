f7d2c8d6133a31b980a0f5c7908f7bcfcd1bf2a0
akashsaini3032 <akashsainira@gmail.com>
2025-12-20 22:37:57 +0530

    mern Project remove

--- Akash/client/src/data/products.js
+++
@@ -1,8 +0,0 @@
-// mock product data (prices in cents)
-const products = [
-  { id: 'p1', title: 'T-Shirt', price: 1999, description: 'Comfortable cotton tee' },
-  { id: 'p2', title: 'Mug', price: 999, description: 'Ceramic coffee mug' },
-  { id: 'p3', title: 'Notebook', price: 1299, description: 'A5 notebook' },
-];
-
-export default products;
--- Akash/server/utils/logger.js
+++
@@ -1,3 +0,0 @@
-module.exports = function log(...args) {
-  console.log('[server]', ...args);
-};
