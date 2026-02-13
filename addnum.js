1d0f7a913413337d0fee7ed9113acfa99370f17b
akashsaini3032 <akashsainira@gmail.com>
2026-02-13 22:45:51 +0530

    javascript practice

--- addnum.js
+++ addnum.js
@@ -1,8 +1,8 @@
 function addNumbers() {
     // this is homework do number lena hai user se and addd karke result dena hai
     //input humesha string ke form me hi recieve hoga isilye number liya
-    let num1 = prompt("Enter the first number:");
-    let num2 = prompt("Enter the second number:");
-    let sum = Number(num1) + Number(num2);
-    alert("The sum is: " + sum);
+    // let num1 = prompt("Enter the first number:");
+    // let num2 = prompt("Enter the second number:");
+    // let sum = Number(num1) + Number(num2);
+    // alert("The sum is: " + sum);
   }
